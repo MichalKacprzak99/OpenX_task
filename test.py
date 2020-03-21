6276a9a8c35c79df3c9d360757b33788208b2d23
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-03-21 21:35:52 +0100

    create some basic test

---
+++ test.py
@@ -0,0 +1,29 @@
+from tree import Tree_methods
+from tree import init_example_tree
+from anytree import AnyNode, RenderTree, PreOrderIter
+import pytest
+
+root = init_example_tree()
+class TestSum:
+    def test_one(self,):
+        assert Tree_methods(root).sum() == 38
+    def test_two(self):
+        assert Tree_methods(root.children[0]).sum() == 10
+    def test_three(self):
+        assert Tree_methods(root.children[1].children[0]).sum() == 1
+
+class TestAverage:
+    def test_one(self):
+        assert Tree_methods(root).average() == 3.8
+    def test_two(self):
+        assert Tree_methods(root.children[0]).average() == 10/3
+    def test_three(self):
+        assert Tree_methods(root.children[1].children[0]).sum() == 1
+
+class TestMediana:
+    def test_one(self):
+        assert Tree_methods(root).mediana() == 4
+    def test_two(self):
+        assert Tree_methods(root.children[0]).mediana() == 3
+    def test_three(self):
+        assert Tree_methods(root.children[1].children[0]).mediana() == 1
